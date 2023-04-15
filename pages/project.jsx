ab7370aeac30e6a201e354a6ddebfaf733224180
Madison Funderburk <mfunderburk@teleflora.com>
2023-04-15 16:40:33 -0500

    adding projects page

---
+++ pages/project.jsx
@@ -0,0 +1,9 @@
+import TopPage from "../components/topPage";
+
+export default function Projects() {
+    return (
+        <TopPage>
+            <h1>Projects coming soon!</h1>
+        </TopPage>
+    );
+}
